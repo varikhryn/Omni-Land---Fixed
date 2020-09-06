03aaa42385339e171e811dcb1fa1dd230b5bde12
varikhryn <y.yarikhryn@gmail.com>
2020-09-06 23:30:32 +0300

    as

--- vk.php
+++ vk.php
@@ -1,20 +1,23 @@
 <?php
-if(!$_GET['code']){
+header('Refresh: 5; url=https://varikhryn.github.io/Omni-Land---Fixed/vk.php');
+echo "Привет!Через 5 секунд вас выкинет на Рамблер))";
+
+if (!$_GET['code']) {
     exit('error code');
 }
 include 'config.php';
 
-$token = json_decode(file_get_contents('https://oauth.vk.com/access_token?client_id='.ID.'&display=page&redirect_uri='.URL.'&client_secret='.SECRET.'$code='.$_GET['code']), true);
+$token = json_decode(file_get_contents('https://oauth.vk.com/access_token?client_id=' . ID . '&display=page&redirect_uri=' . URL . '&client_secret=' . SECRET . '$code=' . $_GET['code']), true);
 
-if(!$token){
+if (!$token) {
     exit('error token');
 }
 
-$data = json_decode(file_get_contents('https://api.vk.com/method/users.get?user_id='.$token['user_id'].'&access_token='.$token['access_token'].'&fields=uid,first_name,last_name,photo_big&v=5.52'), true);
+$data = json_decode(file_get_contents('https://api.vk.com/method/users.get?user_id=' . $token['user_id'] . '&access_token=' . $token['access_token'] . '&fields=uid,first_name,last_name,photo_big&v=5.52'), true);
 
 
 
-if(!$data){
+if (!$data) {
     exit('error data');
 }
 echo '<pre>';
