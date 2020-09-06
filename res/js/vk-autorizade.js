afc47d68ec7282fad0374bc73cdbc67189901708
varikhryn <y.yarikhryn@gmail.com>
2020-09-06 16:34:39 +0300

    vk

---
+++ res/js/vk-autorizade.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    VK.init({ apiId: 7588436 });
+
+    // < !--VK Widget-- >
+    VK.Widgets.Auth("vk_auth", { "authUrl": "/dev/Login" });
+
+
+});
