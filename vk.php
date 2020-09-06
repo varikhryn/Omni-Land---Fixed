bd194e65d7d083a83f58230044b45a9992a9fcfc
varikhryn <y.yarikhryn@gmail.com>
2020-09-06 18:08:05 +0300

    v

--- vk.php
+++ vk.php
@@ -4,10 +4,16 @@ if(!$_GET['code']){
 }
 include 'config.php';
 
-$token = json_decode(file_get_contents('https://oauth.vk.com/access_token?client_id='.ID.'&display=page&redirect_uri='.URL.'&client_secret='.SECRET.')$code='.$_GET['code']'', name)
+$token = json_decode(file_get_contents('https://oauth.vk.com/access_token?client_id='.ID.'&display=page&redirect_uri='.URL.'&client_secret='.SECRET.')$code='.$_GET['code']'', true)
 
 if(!$token){
     exit('error token');
+}
+
+$data = json_decode(file_get_contents('https://api.vk.com/method/users.get?user_id='.ID.'&v=5.52', true)
+
+if(!$data){
+    exit('error data');
 }
 
 var_dump($token);
